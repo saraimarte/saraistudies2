71d2b076bca0276ad204c4052bde487f30eea9f9
Sarai Marte <smartepujols@gmail.com>
2024-02-13 03:12:45 -0500

    playing around w sizes of the screen

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -47,7 +47,7 @@ gltfLoader.setDRACOLoader(dracoLoader)
 
 
 //Desk
-const bakedTexture = textureLoader.load('../../public/textures/deskNew4.png')
+const bakedTexture = textureLoader.load('../../public/textures/desk6.png')
 bakedTexture.flipY = false
 bakedTexture.colorSpace = THREE.SRGBColorSpace
 const bakedMaterial = new THREE.MeshBasicMaterial({ map: bakedTexture })
